3355e92b8e20d33d6402140d015b462b38e93f22
Victor David Garcia Zapata <david.prometeo@gmail.com>
2023-08-26 19:43:53 -0500

    Se adicionan archivos faltantes

---
+++ src/main/java/com/ptm/fullstackbackendPTM/FullstackBackendPtmApplication.java
@@ -0,0 +1,13 @@
+package com.ptm.fullstackbackendPTM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBackendPtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackendPtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptm/fullstackbackendPTM/FullstackBackendPtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptm.fullstackbackendPTM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendPtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
